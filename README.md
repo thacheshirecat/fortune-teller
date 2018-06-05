02c1019bc2dc01d913a6b1bd2aaf4b18e8c009c8
Alex Bunnell and Jean Jia <alex@>
2018-06-05 15:30:24 -0700

    Add styles using CSS.

---
+++ README.md
@@ -0,0 +1,30 @@
+# _Fortune Teller_
+
+#### _A fortune telling website, 06/05/2018_
+
+#### By _**Jean Jia and Alex Bunnell**_
+
+## Description
+
+_This is a fortune telling a website by filling out the survey._
+
+## Setup/Installation Requirements
+
+* _Git clone this folder_
+* _Open the index.html in the browser_
+* _Fill and submit the form_
+* _Your result will be display in the bottom panel_
+
+## Support and contact details
+
+_Please contact Jean84646@gmail.com or for any issues, questions, ideas, or concerns._
+
+## Technologies Used
+
+_HTML, CSS, Bootstrap, JavaScript branching, jQuery functions, Github_
+
+### License
+
+*This software is licensed under the MIT license*
+
+Copyright (c) 2018 **_Jean Jia and Alex Bunnell_**
--- js/scripts.js
+++ js/scripts.js
@@ -12,10 +12,10 @@ $("#luck-input").submit(function(event) {
     });
   $("input:checkbox[name=good-luck-input]:checked").each(function(){
       var goodLuck = parseInt($(this).val());
-      goodLuckScore += goodLuck;
+      goodLuckScore = goodLuckScore + goodLuck;
     });
 
-    if (badLuckScore === 5) {
+    if (badLuckScore === 6) {
       $("#your-fortune").text("Please don't touch me! Your luck is unbelievably bad right now and I am going to need you to leave before there is collateral damage. Get out!!!!");
     } else if (badLuckScore > goodLuckScore) {
       $("#your-fortune").text("Things aren't looking good for you. I would stock up on genuine rabbits foot and consider (even if you have already in the past year) visiting the Blarney stone for some spider-man action.");
